5d45b2966350877459d5a621cc88e50e37cc52f9
ksnyder213 <thisiskatiemae@gmail.com>
2017-10-01 17:52:04 -0700

    for navbar scroll class styles, adds signin styles and navbar hover styles

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
+/* To do:
+(1) fix signin background color on scroll class
+(2) add scroll function to js so classes are added when user scrolls
+
+*/
+
+
 var navbar = document.querySelector("header");
 var navbarLinks = document.querySelectorAll("nav a");
-var signIn = document.getElementById("signin");
+var signIn = document.querySelector(".signin");
 
 
 
@@ -11,4 +18,4 @@ for (var i = 0; i < navbarLinks.length; i++) {
     navbarLinks[i].classList.add("navbar-scroll-links");
 }
 
-signIn.classList.add("signin-scroll")
+signIn.classList.add("signin-scroll");
